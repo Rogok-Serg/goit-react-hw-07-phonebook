3ac8e6b31f7d5cf09e2bc593c1128a2597df9e96
Rogok-Serg <rogoksv@gmail.com>
2023-08-03 01:55:59 +0300

    finish task 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { useSelector } from 'react-redux';
+import { selectContacts } from 'redux/selectors';
+
 export const App = () => {
-  return <div></div>;
+  const contacts = useSelector(selectContacts);
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {contacts.length !== 0 && <Filter />}
+      {contacts.length !== 0 && <ContactList />}
+    </>
+  );
 };
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+// import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectContacts, selectFilter } from 'redux/selectors';
+import { useEffect } from 'react';
+import { deleteContact, fetchContacts } from 'redux/operations';
+
+export const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts);
+  }, [dispatch]);
+
+  const onRemoveContact = contactId => dispatch(deleteContact(contactId));
+
+  const getFilteredContacts = () => {
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  return (
+    <ul className={css.list}>
+      {getFilteredContacts().map(({ id, name, number }) => {
+        return (
+          <li className={css.items} key={id}>
+            <p>{name} </p>
+            <p>: {number}</p>
+            <button type="button" onClick={() => onRemoveContact(id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
